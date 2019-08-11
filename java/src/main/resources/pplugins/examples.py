350e15adae6ff25c87efc1515f5ad952b70cf411
Wensheng Wang <wenshengwang@gmail.com>
2019-08-10 22:07:11 -0500

    add sphere example

--- java/src/main/resources/pplugins/examples.py
+++ java/src/main/resources/pplugins/examples.py
@@ -1,22 +1,24 @@
 import time
-from math import (pi, sin)
-from mcpi.minecraft import mc, mcpy
+from math import (pi, sin, cos)
+from mcpi.minecraft import Minecraft, mcpy
 
 
 @mcpy
 def cube(size=5, typeName='SANDSTONE'):
     "create a cube"
+    mc = Minecraft.create()
     pos = mc.player.getTilePos()
     s = int(size)
     mc.setBlocks(pos.x + 1, pos.y, pos.z,
         pos.x + s, pos.y + s - 1, pos.z + s - 1, typeName)
-    
+
 
 def flatten(size=50):
     """_mcpy: flatten world around me.
     change one layer of blocks below me to sandstone,
     clear everything above.
     """
+    mc = Minecraft.create()
     pos = mc.player.getTilePos()
     s = int(size)
     mc.setBlocks(pos.x - s, pos.y - 1, pos.z - s,
@@ -29,6 +31,7 @@ def falling_block():
     """_mcpy
     A gold block is falling from the sky
     """
+    mc = Minecraft.create()
     pos = mc.player.getTilePos()
     y = pos.y + 40
     for i in range(40):
@@ -46,6 +49,7 @@ def rainbow():
     create a rainbow.
     The code is from: http://dev.bukkit.org/bukkit-plugins/raspberryjuice/
     """
+    mc = Minecraft.create()
     pos = mc.player.getTilePos()
     colors = ['RED_WOOL', 'ORANGE_WOOL', 'YELLOW_WOOL', 'LIME_WOOL', 'LIGHT_BLUE_WOOL', 'BLUE_WOOL', 'PURPLE_WOOL']
     height = 60
@@ -54,3 +58,34 @@ def rainbow():
         for colourindex in range(0, len(colors)):
             y = sin((x / 128.0) * pi) * height + colourindex
             mc.setBlock(pos.x+x - 64, pos.y+y, pos.z, colors[len(colors) - 1 - colourindex])
+
+
+@mcpy
+def sphere(size=10, typeName='STONE'):
+    """create a sphere
+    The xz center of sphere is (size+2) away from player
+    player always look at sphere directly
+    """
+    s = int(size)
+    mc = Minecraft.create()
+    if s < 6 or s > 100:
+        mc.postToChat("size can not be less than 6 or larger than 100")
+        return
+
+    pos = mc.player.getTilePos()
+    rot = mc.player.getRotation()
+    r = rot / 180.0 * pi
+    d = s + 2
+    h = int(s / 2)
+
+    center_x = pos.x - int(d * sin(r))
+    center_z = pos.z + int(d * cos(r))
+    center_y = pos.y + h + 1
+    hh = h * h
+    for i in range(-h, h):
+        ii = i * i
+        for j in range(-h, h):
+            jj = j * j
+            for k in range(-h, h):
+                if ii + jj + k * k < hh:
+                    mc.setBlock(center_x + i, center_y + j, center_z + k, typeName)
