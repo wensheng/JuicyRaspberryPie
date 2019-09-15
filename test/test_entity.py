70992daa6728c35153173a8ed2d5cc565cc8edc6
Wensheng Wang <wenshengwang@gmail.com>
2019-09-14 23:29:31 -0500

    test change

--- test/test_entity.py
+++ test/test_entity.py
@@ -22,57 +22,58 @@ pos = mc.player.getTilePos()
 
 def test_pos():
     # in game make player face south to observe
-    entityId = mc.spawnEntity(pos.x, pos.y, pos.z + 4, "pig")
-    print("ID of the pig:", entityId)
+    pig = mc.spawnEntity(pos.x, pos.y, pos.z + 4, "pig")
+    print("ID of the pig:", pig.id)
     time.sleep(1)
-    print("pic direction:", mc.entity.getDirection(entityId))
+    print("pic direction:", pig.getDirection())
     entities = mc.getNearbyEntities(pos.x, pos.y, pos.z)
     # should print out player and pig
     for entity in entities:
-        print(entity)
+        print(entity.id)
     time.sleep(2)
     # kill the pig by free-falling it
-    mc.entity.setTilePos(entityId, pos.x, pos.y + 50, pos.z + 4)
+    pig.setTilePos(pos.x, pos.y + 50, pos.z + 4)
     time.sleep(3)
 
-    entityId = mc.spawnEntity(pos.x, pos.y, pos.z + 20, "panda")
+    panda = mc.spawnEntity(pos.x, pos.y, pos.z + 20, "panda")
     time.sleep(1)
-    mc.entity.setTilePos(entityId, pos.x, pos.y, pos.z + 15) 
+    panda.setTilePos(pos.x, pos.y, pos.z + 15) 
     time.sleep(1)
-    mc.entity.setTilePos(entityId, pos.x, pos.y, pos.z + 10) 
+    panda.setTilePos(pos.x, pos.y, pos.z + 10) 
     time.sleep(1)
-    mc.entity.setTilePos(entityId, pos.x, pos.y, pos.z + 5) 
+    panda.setTilePos(pos.x, pos.y, pos.z + 5) 
     mc.postToChat("panda will disapear in 5 seconds")
     time.sleep(5)
-    mc.entity.remove(entityId)
+    panda.remove()
 
 
 def test_rotation():
-    entityId = mc.spawnEntity(pos.x, pos.y, pos.z + 4, "boat")
+    boat = mc.spawnEntity(pos.x, pos.y, pos.z + 4, "boat")
     time.sleep(2)
-    print("last boat rotation:", mc.entity.getRotation(entityId))
+    print("last boat rotation:", boat.getRotation())
     for i in range(50):
         angle = (i % 8) * 45
-        mc.entity.setRotation(entityId, angle)
+        boat.setRotation(angle)
         time.sleep(0.2)
 
-    print("current boat rotation:", mc.entity.getRotation(entityId))
+    print("current boat rotation:", boat.getRotation())
     time.sleep(3)
-    mc.entity.remove(entityId)
+    boat.remove()
 
 
 def test_pitch():
+    # use mc.entity.xx instead of entity.xx
     # don't know which entity can bend head -90-90 degree
-    entityId = mc.spawnEntity(pos.x, pos.y, pos.z + 3, "llama")
+    llama = mc.spawnEntity(pos.x, pos.y, pos.z + 3, "llama")
     time.sleep(2)
-    print("last witch pitch:", mc.entity.getPitch(entityId))
+    print("last witch pitch:", mc.entity.getPitch(llama.id))
     for i in range(24):
         angle = (i % 12) * 15 - 90
-        mc.entity.setPitch(entityId, angle)
+        mc.entity.setPitch(llama.id, angle)
         time.sleep(0.2)
-    print("current witch pitch:", mc.entity.getPitch(entityId))
+    print("current witch pitch:", mc.entity.getPitch(llama.id))
     time.sleep(3)
-    mc.entity.remove(entityId)
+    mc.entity.remove(llama.id)
     
 
 def main():
