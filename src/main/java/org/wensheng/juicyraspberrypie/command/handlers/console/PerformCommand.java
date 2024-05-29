86e7116d1527f84d9ea6936a30b76a67e4a53cd0
Ingo Karkat <ingo@resilient-group.de>
2024-05-29 17:30:41 +0200

    TASK-4121 ENH: Secure console command execution via a whitelist of regular expression patterns
    The console has admin rights and can impersonate any player, too. As mcpi itself doesn't provide any authentication, we need to limit what can be executed. A whitelist is more secure than blacklisting insecure commands.

--- src/main/java/org/wensheng/juicyraspberrypie/command/handlers/console/PerformCommand.java
+++ src/main/java/org/wensheng/juicyraspberrypie/command/handlers/console/PerformCommand.java
@@ -1,21 +1,41 @@
 package org.wensheng.juicyraspberrypie.command.handlers.console;
 
 import org.bukkit.Bukkit;
+import org.jetbrains.annotations.NotNull;
 import org.wensheng.juicyraspberrypie.command.Handler;
 import org.wensheng.juicyraspberrypie.command.Instruction;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
 
 /**
  * Execute the given command on the console.
  */
 public class PerformCommand implements Handler {
 	/**
-	 * Create a new PerformCommand event handler.
+	 * The configured whitelist patterns for commands.
+	 * Anchored Java regular expressions; at least one must match for a command to be allowed.
 	 */
-	public PerformCommand() {
+	@NotNull
+	private final List<Pattern> whitelistPatterns;
+
+	/**
+	 * Create a new PerformCommand event handler.
+	 *
+	 * @param whitelistPattern The whitelist patterns to associate with this handler.
+	 */
+	public PerformCommand(@NotNull final List<String> whitelistPattern) {
+		this.whitelistPatterns = Objects.requireNonNull(whitelistPattern).stream().map(Pattern::compile).toList();
 	}
 
 	@Override
-	public String handle(final Instruction instruction) {
-		return String.valueOf(Bukkit.dispatchCommand(Bukkit.getConsoleSender(), instruction.next()));
+	public String handle(@NotNull final Instruction instruction) {
+		final String command = instruction.next();
+		if (whitelistPatterns.stream().noneMatch(pattern -> pattern.matcher(command).matches())) {
+			return "Fail: Command rejected: " + command;
+		}
+
+		return String.valueOf(Bukkit.dispatchCommand(Bukkit.getConsoleSender(), command));
 	}
 }
